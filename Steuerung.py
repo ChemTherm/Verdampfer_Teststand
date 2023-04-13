917a742587502aaee80d09d802943a857536b917
Christian Walter Fujitsu <christian.walter@chem-therm.de>
2023-04-13 14:37:39 +0200

    updates

--- Steuerung.py
+++ Steuerung.py
@@ -8,44 +8,52 @@ from ChemTherm_library.Verdampfer_lib import *
 
 
 def tk_loop():   
-      
-    if running_Json == 1:
-        T_set, MFC_set, t_end,section, t_section = json_timing(config,section,t0)
+    global section
+
+    if running_json == 1:
+        T_set, MFC_set, t_end, section, t_section = json_timing(config,section,t0)
         lable_timer.configure(text = str("{0:.2f}").format(t_end/60)+" min")
         lable_timerSection.configure(text = str("{0:.2f}").format(t_section/60)+" min")
-        for i in set_T:
+        print(T_set)
+        
+        for num, i in enumerate(option_Heat):
             set_T[i].delete(0, tk.END)
-            set_T[i].insert(0,str("{0:.2f}").format(T_set[i]))
+            set_T[i].insert(0,str("{0:.2f}").format(T_set[num]))
 
-        for i in set_MFC:
+        
+        for num, i in enumerate(option_Heat):
             set_MFC[i].delete(0, tk.END)
-            set_MFC[i].insert(0,str(MFC_set[i]))
+            set_MFC[i].insert(0,str(MFC_set[num]))
+            
         getdata()         
         if (t_end < 0):
             Stop_Button_callback()  
 
+    pressure1.get()
+    pressure2.get()
     for tc_obj_name in tc_list:
         lable_T_ist[tc_obj_name].configure(text = str(tc_list[tc_obj_name].t)+" °C")
     for p_obj_name in Heater_list:   
         progressbar[p_obj_name].set(Heater_list[p_obj_name].pwroutput/100)
+    for press_obj_name in pressure_list:
+        value_pressure[press_obj_name].configure(text = str(pressure_list[press_obj_name].Voltage) + " mV")
 
     MFC_1.get()
-    value_MFC[0].configure(text = str(MFC_1.Voltage) + " mV") 
-    pressure1.get()
-    pressure2.get()
-    value_pressure1.config(text = str(pressure1.Voltage) + " mV") 
-    value_pressure2.config(text = str(pressure2.Voltage) + " mV") 
+    value_MFC[option_MFC[0]].configure(text = str(MFC_1.Voltage) + " mV")     
 
     Heater1.regeln()
     window.after(500, tk_loop)
 
 def getdata():    
+    
+    for num, i in enumerate(option_Heat):
+        if set_T[i].get() != '':
+            Heater1.set_t_soll(float(set_T[i].get()))
 
-    if set_T[0].get() != '':
-        Heater1.set_t_soll(float(set_T[0].get()))
-
-
-    MFC_1.set(int(set_MFC[0].get()))
+    
+    for num, i in enumerate(option_MFC):
+        if set_MFC[i].get() != '':
+            MFC_1.set(int(set_MFC[i].get()))
 
 def Start_Button_callback():
     global section, running_json, t0
@@ -68,7 +76,7 @@ SETUP
 '''
 t0 = time.time()
 section = 0
-running_Json = 0
+running_json = 0
 # Set Devices
 HOST = "localhost"
 PORT = 4223
@@ -109,6 +117,7 @@ pressureDual1 = TF_IndustrialDualAnalogIn(ipcon, "23UA")
 pressureDual2 = TF_IndustrialDualAnalogIn(ipcon, "24xB")
 pressure1 = pressure(pressureDual1,0)
 pressure2 = pressure(pressureDual1,1)
+pressure_list = {'p1':pressure1,'p2':pressure2}
 
 MFC_WaterV = TF_IndustrialDualAnalogIn(ipcon, "23Ud")
 
@@ -129,8 +138,8 @@ window.title(config['EVAP']['Name'])
 window.configure(bg= config['TKINTER']['background-color'])
 window.attributes('-fullscreen',True)
 #----------- Images ----------- 
-bg_image = ctk.CTkImage(Image.open(config['Background']['name']))
-close_img = ctk.CTkImage(Image.open(config['PATH']['images'] + 'close.png'),size=(80, 80))
+bg_image = ctk.CTkImage(Image.open(config['TKINTER']['name']),size=(159, 659))
+close_img = ctk.CTkImage(Image.open('./img/close.png'),size=(80, 80))
 
 
 #----------- Frames ----------
@@ -147,28 +156,17 @@ label_background.place(x = x_offset,y = y_offset)
 label_background.lower()
 
 
-lf_pressure = ctk.CTkFrame(window, text='Druck')
+lf_pressure = ctk.CTkFrame(window, border_color=config['TKINTER']['background-color'], border_width=0, height=scrH, width=scrW)
+pressure_Frame = ctk.CTkLabel(lf_MFC, font = ('Arial',16), text='Druck')
 lf_pressure.grid(column=3, row=2, padx=20, pady=20)
 lf_pressure.place(x= 1050,y= 840)
-
-
-Lable_pressure1= tk.Label(lf_pressure, text='Druck_1 ')
-Lable_pressure1.grid(column=0, row=0, ipadx=5, ipady=5)
-value_pressure1= tk.Label(lf_pressure, text='NaN mV')
-value_pressure1.grid(column=1, row=0, ipadx=5, ipady=5)
-
-Lable_pressure2= tk.Label(lf_pressure, text='Druck_2 ')
-Lable_pressure2.grid(column=0, row=0, ipadx=5, ipady=5)
-value_pressure2= tk.Label(lf_pressure, text='NaN mV')
-value_pressure2.grid(column=1, row=0, ipadx=5, ipady=5)
-
-
 
 lf_control = tk.LabelFrame(window, text='Steuerung')
 lf_control.grid(column=0, row=1, padx=20, pady=20)
 
 
 option = ['T1','T2','T3']
+option_pressure = ['p1','p2']
 option_Heat = ['H1']
 option_MFC = ['H1']
 
@@ -183,19 +181,28 @@ for num, i in enumerate(option_Heat):
     progressbar[i] = ctk.CTkProgressBar(master=window, width = 80, progress_color = 'red')
     progressbar[i] .place(x = x_offset + config['T-Value']['x'][num]-8,y = y_offset + config['T-Value']['y'][num]+30)
     set_T[i] = tk.Entry(window, font = ('Arial',16), width = 6,bg='light blue' )
-    set_T[i].place(x = x_offset + config['T-Set']['x'][i],y = y_offset + config['T-Set']['y'][i])
+    set_T[i].place(x = x_offset + config['T-Set']['x'][num],y = y_offset + config['T-Set']['y'][num])
+
+lable_pressure = {}; value_pressure={}
+
+for num, i in enumerate(option_pressure):
+    lable_pressure[i]= ctk.CTkLabel(lf_pressure, font = ('Arial',16), text=option_pressure[num])
+    lable_pressure[i].grid(column=0, row=num+1, ipadx=5, ipady=5)
+    value_pressure[i]= ctk.CTkLabel(lf_pressure, font = ('Arial',16), text='0 mV')
+    value_pressure[i].grid(column=3, row=num+1, ipadx=5, ipady=5)
+
 
 name_MFC={}; set_MFC={}; unit_MFC={}; value_MFC={}
 
-for num, i in enumerate(option_Heat):
-    name_MFC[i]= ctk.CTkLabel(lf_MFC, font = ('Arial',16), text=config['MFC']['name'][i])
-    name_MFC[i].grid(column=0, row=i+1, ipadx=5, ipady=5)
+for num, i in enumerate(option_MFC):
+    name_MFC[i]= ctk.CTkLabel(lf_MFC, font = ('Arial',16), text=config['MFC']['name'][num])
+    name_MFC[i].grid(column=0, row=num+1, ipadx=5, ipady=5)
     set_MFC[i] = tk.Entry(lf_MFC, font = ('Arial',16), width = 6 )
-    set_MFC[i].grid(column=1, row=i+1, ipadx=5, ipady=5)
+    set_MFC[i].grid(column=1, row=num+1, ipadx=5, ipady=5)
     unit_MFC[i]= ctk.CTkLabel(lf_MFC, font = ('Arial',16), text=' mV')
-    unit_MFC[i].grid(column=2, row=i+1, ipadx=5, ipady=5)
+    unit_MFC[i].grid(column=2, row=num+1, ipadx=5, ipady=5)
     value_MFC[i]= ctk.CTkLabel(lf_MFC, font = ('Arial',16), text='0 mV')
-    value_MFC[i].grid(column=3, row=i+1, ipadx=5, ipady=5)
+    value_MFC[i].grid(column=3, row=num+1, ipadx=5, ipady=5)
 
 lable_timer = ctk.CTkLabel(master = lf_control , font = ('Arial',16), text='0 min')
 lable_timer.grid(column=1, row=2)
